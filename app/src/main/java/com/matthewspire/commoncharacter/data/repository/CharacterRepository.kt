51928b654e0e9bf9a6564e1a3478c4497a0193a0
Matthew Spire <matthew.t.spire@gmail.com>
2023-04-27 08:49:47 -0700

    Fixed issue with theWire variant.

--- app/src/main/java/com/matthewspire/commoncharacter/data/repository/CharacterRepository.kt
+++ app/src/main/java/com/matthewspire/commoncharacter/data/repository/CharacterRepository.kt
@@ -1,5 +1,6 @@
 package com.matthewspire.commoncharacter.data.repository
 
+import android.util.Log
 import com.matthewspire.commonCharacter.BuildConfig
 import com.matthewspire.commoncharacter.data.api.ApiService
 import com.matthewspire.commoncharacter.data.model.ApiResult
@@ -23,24 +24,39 @@ class CharacterRepository(private val apiService: ApiService) {
     // @param apiResult: The API result to be processed.
     // @return: A list of Character objects.
     private fun processResponse(apiResult: ApiResult): List<Character> {
+        return try {
+            // Iterate through the related topics and map each topic to a Character object
+            // or null if the topic does not meet the expected format.
+            apiResult.relatedTopics.mapNotNull { relatedTopic ->
 
-        // Maps the related topics returned by the API to Character objects.
-        return apiResult.relatedTopics.mapNotNull { relatedTopic ->
+                // Split the related topic text by " - " to separate the name and description.
+                val splitText = relatedTopic.text.split(" - ")
 
-            // Extracts the name and description of the character from the related topic.
-            val name = relatedTopic.text.split(" - ")[0]
-            val description = relatedTopic.text.split(" - ")[1]
+                // Check if the split text contains at least two elements (name and description).
+                if (splitText.size >= 2) {
+                    val name = splitText[0]
+                    val description = splitText[1]
 
-            // If both the name and description are not blank, creates a new Character object.
-            if (name.isNotBlank() && description.isNotBlank()) {
-                Character(
-                    name = name,
-                    description = description,
-                    icon = relatedTopic.icon
-                )
-            } else {
-                null
+                    // If both the name and description are not blank, create a new Character object.
+                    if (name.isNotBlank() && description.isNotBlank()) {
+                        Character(
+                            name = name,
+                            description = description,
+                            icon = relatedTopic.icon
+                        )
+                    } else {
+                        null
+                    }
+                } else {
+                    // Log a warning if the related topic's text is in an unexpected format.
+                    Log.w("CharacterRepository", "Skipping related topic due to unexpected format: ${relatedTopic.text}")
+                    null
+                }
             }
+        } catch (exception: Exception) {
+            // Log an error if there was an issue processing the response and rethrow the exception.
+            Log.e("CharacterRepository", "Error processing response: $exception")
+            throw exception
         }
     }
 }
